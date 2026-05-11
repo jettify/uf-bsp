f12ea67182cd64c52bc2e0fdb2a6f67bdc92dfee
Nikolay Novik <nickolainovik@gmail.com>
2026-05-11 19:26:00 -0400

    feat: Hardware veify first tbs example.

---
+++ tbs-lucid-h7-bsp/README.md
@@ -0,0 +1,3 @@
+# tbs-lucid-h7-bsp
+
+Embassy-based BSP crate for the TBS Lucid H7 flight controller.
--- tbs-lucid-h7-bsp/examples/blinky.rs
+++ tbs-lucid-h7-bsp/examples/blinky.rs
@@ -4,22 +4,28 @@
 use bsp::hal::gpio::Level;
 use bsp::hal::gpio::Output;
 use bsp::hal::gpio::Speed;
+use cortex_m as _;
+use defmt::info;
+use defmt_rtt as _;
 use embassy_time::Duration;
 use embassy_time::Timer;
-use panic_halt as _;
+use panic_probe as _;
 use tbs_lucid_h7_bsp as bsp;
 
 #[embassy_executor::main]
 async fn main(_spawner: embassy_executor::Spawner) {
+    info!("starting blinky example");
     let p = bsp::hal::init(bsp::config());
     let board = bsp::Board::new(p);
 
     let mut led0 = Output::new(board.leds.led0, Level::Low, Speed::Low);
     let mut led1 = Output::new(board.leds.led1, Level::High, Speed::Low);
+    info!("leds initialized");
 
     loop {
         led0.toggle();
         led1.toggle();
+        info!("leds toggled");
         Timer::after(Duration::from_millis(250)).await;
     }
 }
